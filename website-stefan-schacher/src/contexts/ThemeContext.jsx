27932cb228b9b7fbed2075f6bcd3e9fa5ad26360
imkehoegden <imke.hoegden@posteo.de>
2026-03-12 12:11:47 +0100

    Feat: ThemeContext erstellt

--- website-stefan-schacher/src/contexts/ThemeContext.jsx
+++ website-stefan-schacher/src/contexts/ThemeContext.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
 
-export default function ThemeContext() {
-  return <div>ThemeContext</div>;
+export const ThemeContext = createContext();
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(() => {
+    const saved = localStorage.getItem("theme");
+    return saved ? JSON.parse(saved) : "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.className = theme; // so reagieren meine Tailwind-Klassen wie dark:bg-gray-900 direkt, ohne dass ich extra etwas an den Komponenten ändern muss
+    localStorage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
