a8de852639ba9979a8776bc67ee131ac7533647a
imkehoegden <imke.hoegden@posteo.de>
2026-03-16 14:26:14 +0100

    style: footer padding adjusted

--- website-stefan-schacher/src/components/Footer.jsx
+++ website-stefan-schacher/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const { language } = useContext(LanguageContext);
 
   return (
-    <footer className="w-full mx-auto px-8 pb-20 flex flex-col md:flex-col lg:flex-row tracking-widest justify-between items-center font-light text-gray-500 sm:gap-4 md:gap-4">
+    <footer className="w-full mx-auto px-8 py-20 flex flex-col md:flex-col lg:flex-row tracking-widest justify-between items-center font-light text-gray-500 sm:gap-4 md:gap-4">
       <div>© STEFAN SCHACHER {new Date().getFullYear()}</div>
 
       <div className="flex flex-col md:flex-col lg:flex-row lg:gap-6 justify-between items-center sm:gap-4 md:gap-4">
