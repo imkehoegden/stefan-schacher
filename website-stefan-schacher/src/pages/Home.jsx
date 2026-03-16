38e8e830bbf362ac00a90c98aa81fc730b3fbfbc
imkehoegden <imke.hoegden@posteo.de>
2026-03-16 13:54:04 +0100

    Add: Language Context implemented on Button

--- website-stefan-schacher/src/pages/Home.jsx
+++ website-stefan-schacher/src/pages/Home.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Gallery from "../components/Gallery";
 import Carousel from "../components/Carousel";
 import { carouselImages } from "../data/imagesCarouselHome";
 import Button from "../components/Button";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 export default function Home() {
   const navigate = useNavigate();
+  const { language } = useContext(LanguageContext);
 
   return (
     <>
       <Carousel pics={carouselImages} interval={7000} />
       <Gallery />
-      <Button onClick={() => navigate("/work")}>Work</Button>
+      <Button onClick={() => navigate("/work")}>
+        {language === "de" ? "PORTFOLIO" : "WORK"}
+      </Button>
     </>
   );
 }
